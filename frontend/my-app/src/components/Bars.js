e0541635c0165c8fe5f0d39851c2f5fbcfdce3cb
sabrina-xing <xing.sabi@gmail.com>
2023-01-21 14:53:10 -0500

    new frontend components

---
+++ frontend/my-app/src/components/Bars.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+// copied code for a progress bar but it is supposed
+//   supposed to move with a button
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+}
