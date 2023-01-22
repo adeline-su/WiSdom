cfe99eccd242c583b879f2abb30ca1f6bd94942b
liya-zhu <julia.liya.zhu@gmail.com>
2023-01-22 02:18:01 -0500

    added posting review function that doesn't rly post yet

--- frontend/my-app/src/components/Company.js
+++ frontend/my-app/src/components/Company.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate, redirect } from "react-router-dom"
-import Review from './Review';
+import PostReviewBox from './PostReview';
+// import ReviewHistory from 'ReviewHistory';
 
 
 const Company = () => {
@@ -10,16 +11,26 @@ const Company = () => {
     return (
         <div>
             <p>Hello, this is a Company Profile page</p>
-            <Link to ='/review'>
+            {/* <Link to ='/review'>
                 <button className='newReview'>
                     Create a New Review
                 </button>
-            </Link>
+            </Link> */}
             <Link to ='/'>
                 <button className='backhome'> 
                     Take me back home
                 </button>
             </Link>
+
+            
+            <div> {/* review section */}
+                <PostReviewBox/>
+
+                review history
+            </div>
+
+            
+                
         </div>
         
     )
