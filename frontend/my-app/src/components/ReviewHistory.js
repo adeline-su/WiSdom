aaf9011461aabdb5e2d5b81e5578bd1d19df3a22
liya-zhu <julia.liya.zhu@gmail.com>
2023-01-22 03:10:01 -0500

    add review box again

---
+++ frontend/my-app/src/components/ReviewHistory.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, useNavigate, redirect } from "react-router-dom";
+
+const ReviewHistory = () => {
+
+      
+    return ( 
+     <div>
+          pull from fire base idk
+     </div>
+     );
+}
